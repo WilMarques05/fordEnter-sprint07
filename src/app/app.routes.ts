7ba5a017bd3fdcab2cb0a3d352f8832828134e8f
WILLIS SILVA MARQUES <014.450864@universidadesenaicimatec.edu.br>
2026-06-16 11:13:48 -0300

    feat: estruturar projeto criando components, estruturando rotas, adicionando bootstrap e realizando suas configurações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/login/login.component').then(
+        (c) => c.LoginComponent,
+      );
+    },
+  },
+
+  {
+    path: 'home',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/home/home.component').then((c) => c.HomeComponent);
+    },
+  },
+
+  {
+    path: 'deashboard',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/deashboard/deashboard.component').then(
+        (c) => c.DeashboardComponent,
+      );
+    },
+  },
+];
---
+++ src/app/pages/deashboard/deashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deashboard',
+  imports: [],
+  templateUrl: './deashboard.component.html',
+  styleUrl: './deashboard.component.css'
+})
+export class DeashboardComponent {
+
+}
