2a4ab2afa3b7fa8873d0347c755e93f28d604cbf
WILLIS SILVA MARQUES <014.450864@universidadesenaicimatec.edu.br>
2026-06-18 11:53:05 -0300

    feat: adicionar obsevable na requisição

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 import { Usuario } from '../models/user';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { Usuario } from '../models/user';
 export class LoginService {
   http = inject(HttpClient);
 
-  fazerLogin(nome: string, senha:string){
+  fazerLogin(nome: string, senha: string): Observable<Usuario>{
     return this.http.post<Usuario>(
       "http://localhost:3001/login",
       {nome, senha}
