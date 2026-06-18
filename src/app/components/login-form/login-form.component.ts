3d916daabf70afc64dbae344a3751c2ce1e5c35c
WILLIS SILVA MARQUES <014.450864@universidadesenaicimatec.edu.br>
2026-06-18 11:54:12 -0300

    feat: continuar logica para login

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login-form',
   imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+
+  loginService = inject(LoginService)
+
   loginForm = new FormGroup({
-    nome: new FormControl("", Validators.required),
-    senha: new FormControl("", Validators.required)
+    nome: new FormControl<string>('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+    senha: new FormControl('', Validators.required),
   });
 
-  login(){}
+  async login() {
+    const {nome, senha} = this.loginForm.value;
+    if (this.loginForm.valid && nome && senha) {
+      await this.loginService.fazerLogin(nome, senha);
+      this.loginForm.reset();
+    }
+  }
 }
