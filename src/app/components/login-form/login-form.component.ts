43eae23b90ac43dae307be4bd629e84e44080bc3
WILLIS SILVA MARQUES <014.450864@universidadesenaicimatec.edu.br>
2026-06-19 11:48:35 -0300

    feat: implementar mensagem de erro personalizado na tela

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login-form',
@@ -14,19 +15,41 @@ import { LoginService } from '../../services/login.service';
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
-
-  loginService = inject(LoginService)
+  loginService = inject(LoginService);
+  spinnerAguardando = false;
+  mensagemErro = '';
 
   loginForm = new FormGroup({
-    nome: new FormControl<string>('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+    nome: new FormControl<string>('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z ]*'),
+    ]),
     senha: new FormControl('', Validators.required),
   });
 
   async login() {
-    const {nome, senha} = this.loginForm.value;
+    const { nome, senha } = this.loginForm.value;
+
     if (this.loginForm.valid && nome && senha) {
-      await this.loginService.fazerLogin(nome, senha);
-      this.loginForm.reset();
+      try {
+        this.spinnerAguardando = true;
+        await firstValueFrom(this.loginService.fazerLogin(nome, senha));
+        alert('Login Efetuado com Sucesso! ' + `Bem vindo, ${nome}!`);
+        this.loginForm.reset();
+      } catch (error: any) {
+        if (error.status === 401) {
+          this.mensagemErro = 'Usuário ou Senha incorreto!';
+        } else if (error.status === 500 || error.status === 0) {
+          this.mensagemErro =
+            'Não foi possível se comunicar ao servidor, tente mais tarde!';
+        } else {
+          this.mensagemErro = 'Erro ao realizar login, tente mais tarde' + error.status + ' ' + error.message;
+        }
+      } finally {
+        this.spinnerAguardando = false;
+      }
+    } else {
+      this.mensagemErro = 'reveja os campos e tente novamente!';
     }
   }
 }
